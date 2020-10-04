9c9749873856a941a175cc56dc3fb3392a608863
Asif Ahmed <rumon0016@gmail.com>
2020-10-04 17:30:08 +0600

    Email

--- ssms/settings.py
+++ ssms/settings.py
@@ -119,8 +119,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sharmin.ruma0016@gmail.com'
-EMAIL_HOST_PASSWORD = 'ruma@sharmin'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 # Internationalization
