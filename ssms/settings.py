965903ef6060d37040577490091f5ae90764ad32
Asif Ahmed <rumon0016@gmail.com>
2020-10-17 13:02:02 +0600

    excluding celery

--- ssms/settings.py
+++ ssms/settings.py
@@ -116,14 +116,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Dhaka'
-
+# # CELERY STUFF
+# BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Asia/Dhaka'
 
 
 EMAIL_USE_TLS = True
